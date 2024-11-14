46ac1195bef8be175186902881a314633423cbcf
matiterra <matiasterranova1775@gmail.com>
2024-11-14 01:39:31 -0300

    le meti javascript

--- tus_puntos/Untitled-2.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-
-    const progressBar = document.querySelector('.progress-bar');
-    progressBar.style.width = '20%'; // Adjust to reflect current points
-});
